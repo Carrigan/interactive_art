28c49ee719d56b445b386e3bf0258f823faaaf4a
Carrigan <brian.c.carrigan@gmail.com>
2016-09-11 20:30:05 -0400

    Move the mock to the Arduino module; use on connection failure.

--- modern_art/src/modern_art/arduino.clj
+++ modern_art/src/modern_art/arduino.clj
@@ -1,5 +1,6 @@
 (ns modern-art.arduino
-  (:require [serial-port :as ser]))
+  (:require [serial-port :as ser]
+            [modern-art.mock :as mock]))
 
 (defn on-receive-gen
   [received-atom]
@@ -8,17 +9,30 @@
       (reset! received-atom [])
       (swap! received-atom conj character))))
 
+(defn connect [port-name]
+  (try
+    (ser/open port-name)
+    (catch Exception _ nil)))
 
-(defn initialize
-  [port-name]
-  (let [serial        (ser/open port-name)
-        received-atom (atom [])]
+(defn initialize-port [serial]
+  (let [received-atom (atom [])]
     (ser/on-byte serial (on-receive-gen received-atom))
     (ser/write-int serial (int 0))
-    { :received-atom received-atom
+    { :is-mock false
+      :received-atom received-atom
       :port serial
       :state { :hue 0 :sat 0 :bri 0 :next-button false }
     }))
+
+(defn initialize-mock []
+  { :is-mock true
+    :mock (mock/init)
+    :state { :hue 0 :sat 0 :bri 0 :next-button false }})
+
+(defn initialize
+  [port-name]
+  (let [port (connect port-name)]
+    (if port (initialize-port port) (initialize-mock))))
 
 (defn build-state
   [[hue sat bri buttons _]]
@@ -28,7 +42,7 @@
     :next-button false
   })
 
-(defn update-state
+(defn update-serial
   [state]
   (let [received-atom (:received-atom state)
         message       @received-atom]
@@ -37,3 +51,32 @@
         (reset! received-atom [])
         (assoc state :state (build-state message)))
       state)))
+
+(defn mouse-pressed [state event]
+  (let [mock-state (:mock state)]
+    (assoc state :mock (mock/check-click mock-state event))))
+
+(defn slider-val [state key]
+  (int (* 255 (mock/get-value state key))))
+
+(defn update-mock
+  [state]
+  (let [mock-state (:mock state)
+        hue (slider-val mock-state :hue-slider)
+        sat (slider-val mock-state :sat-slider)
+        bri (slider-val mock-state :bri-slider)
+        next-button (mock/clicked? mock-state :next-btn)]
+    (-> state
+        (assoc :state { :hue hue :sat sat :bri bri :next-button next-button})
+        (update :mock mock/clear-clicks))))
+
+(defn update-state
+  [state]
+  (if (:is-mock state)
+    (update-mock state)
+    (update-serial state)))
+
+(defn render
+  [state]
+  (when (:is-mock state)
+    (mock/render (:mock state))))
--- modern_art/src/modern_art/dynamic.clj
+++ modern_art/src/modern_art/dynamic.clj
@@ -1,55 +1,49 @@
 (ns modern-art.dynamic
   (:require [quil.core :as q]
             [modern-art.lines-and-boxes :as frame]
-            [modern-art.mock :as mock]
             [modern-art.arduino :as arduino]))
 
 (defn setup []
   (q/frame-rate 30)
   (q/color-mode :hsb)
-  {:mock (mock/init)
-   :frames '()
-   :arduino (arduino/initialize "COM3")})
-
-(defn slider-val [state key]
-  (int (* 255 (mock/get-value (:mock state) key))))
-
-(defn frame-from-mock [state]
-  [(slider-val state :hue-slider) (slider-val state :sat-slider) (slider-val state :bri-slider)])
+  {:frames '()
+   :arduino (arduino/initialize "COM9")})
 
 (defn frame-from-arduino [state]
-  [(get-in state [:arduino :state :hue]) (get-in state [:arduino :state :sat]) (get-in state [:arduino :state :bri])])
+  [ (get-in state [:arduino :state :hue])
+    (get-in state [:arduino :state :sat])
+    (get-in state [:arduino :state :bri])
+  ])
 
 (defn recurse-if-clicked [state clicked?]
   (if clicked?
-    (let [cframe (frame-from-arduino state)
-          frames (concat (:frames state) [cframe])]
-      (assoc state :frames frames))
+    (let [current-frame (frame-from-arduino state)
+          all-frames (concat (:frames state) [current-frame])]
+      (assoc state :frames all-frames))
     state))
 
 (defn update-state [state]
   (-> state
-    (recurse-if-clicked (mock/clicked? (:mock state) :next-btn))
-    (assoc :mock (mock/clear-clicks (:mock state)))
-    (update-in [:arduino] arduino/update-state)))
+    (update :arduino arduino/update-state)
+    (recurse-if-clicked (get-in state [:arduino :state :next-button]))))
 
 (defn mouse-pressed [state event]
-  (assoc state :mock (mock/check-click (:mock state) event)))
+  (assoc state :arduino (arduino/mouse-pressed (:arduino state) event)))
 
 (defn recurse-frame [colors [cx cy mx my] [x y]]
   (when-not (empty? colors)
     (let [size-x (- mx cx)
           size-y (- my cy)]
-        (q/push-matrix)
-        (q/translate cx cy)
-        (q/scale (/ size-x x) (/ size-y y))
-        (let [next-location (-> colors (first) (frame/render))]
-          (recurse-frame (rest colors) next-location [x y])
-          (q/pop-matrix)))))
+      (q/push-matrix)
+      (q/translate cx cy)
+      (q/scale (/ size-x x) (/ size-y y))
+      (let [next-location (-> colors (first) (frame/render))]
+        (recurse-frame (rest colors) next-location [x y])
+        (q/pop-matrix)))))
 
 (defn draw-state [state]
   (let [cframe (frame-from-arduino state)
         frames (concat (:frames state) [cframe])]
     (q/background 240)
     (recurse-frame frames [0 0 (q/width) (q/height)] [(q/width) (q/height)])
-    (mock/render (:mock state))))
+    (arduino/render (:arduino state))))
